101b3a5b0481a928a4148ae68e9ce2e7222d2fd8
Pimlanon <nonlaphan.pim@gmail.com>
2023-05-24 02:29:49 +0700

    add more link

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// --------- completed ---------------------
-// document.addEventListener('mousemove', parallax);
-// function parallax(e) {
-//     document.querySelectorAll(".object").forEach(function(move){
-
-//         let moving_value = move.getAttribute('data-value');
-//         let x = e.clientX * moving_value /250;
-//         let y = e.clientY * moving_value /250;
-
-//         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
-//     });
-// }
-
 
 ScrollReveal({
     reset: true,
@@ -23,7 +10,6 @@ ScrollReveal({
   ScrollReveal().reveal('.aboutme-text', { delay: 200 , origin: 'right'});
   ScrollReveal().reveal('.pj figure img', { delay: 200 , origin: 'left' });
   ScrollReveal().reveal('.link', { delay: 200 , origin: 'right' });
-
   ScrollReveal().reveal('.stack img', { origin: 'bottom', interval: 200 });
   ScrollReveal().reveal('.overlay', { delay: 0 , origin: 'right' });
   ScrollReveal().reveal('.flex', { delay: 0 , origin: 'bottom' });
@@ -34,7 +20,10 @@ ScrollReveal({
   ScrollReveal().reveal('#flash-1', { delay: -200 , origin: 'right' });
   ScrollReveal().reveal('#flash-2', { delay: -200 , origin: 'left' });
   ScrollReveal().reveal('#note-1, #note-2, #note-3', { delay: -200 , origin: 'bottom' });
+  ScrollReveal().reveal('.contact-subcon2', { delay: 300 , origin: 'right' });
+  ScrollReveal().reveal('.contact-img', { delay: 300 , origin: 'left' });
 
+  // --------------------- paralax --------------------
   document.addEventListener('mousemove', parallax);
   function parallax(e) {
       document.querySelectorAll(".object").forEach(function(move){
@@ -47,31 +36,9 @@ ScrollReveal({
       });
   }
 
+  // --------------------- scroll top --------------------
   function topFunction() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
-  $(document).ready(function() {
-
-    $('.allContainer-btn').click(function() {
-
-        TweenMax.to('.allContainer-btn', 1, {
-            y: -100,
-            opacity: 0
-        });
-
-        TweenMax.to('.allContainer-text', 1, {
-            y: -400,
-            opacity: 0,
-            ease: Power2.easeInOut,
-            delay: 0.5
-        });
-
-        TweenMax.to('.allContainer', 2, {
-            top: '-110%',
-            delay: 1,
-            ease: Expo.easeInOut,
-        });
-
-    })
-  });
+  
